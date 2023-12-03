a7c6b23855f5f36f1b1548d1fff75337d5dbbb4f
Magnus <150977273+1000Ball@users.noreply.github.com>
2023-12-03 17:03:21 -0500

    Add files via upload

--- Square_Roots.py
+++ Square_Roots.py
@@ -1,6 +1,7 @@
 '''Two methods for taking the square root of a number'''
 
 
+## Slower of the two methods. Coded for fun and proof to self it works
 def sqrt_ld(n, decimal):
     '''Taking the square root of a number n via long division method. Decimal
     specifies what decimal place to go to. Python output is limited to 20 digits.'''
@@ -13,24 +14,40 @@ def sqrt_ld(n, decimal):
     extra_divisor = 0
     add_zeros = 0
     next_digit = None
-    low = 1
+    power = 0
 
-
+    ## Only real special case
+    if n==0:
+        return 0.0
+    
+    ## Checking if it is a whole number
     if n%1 != 0:
         while n%1 != 0:
             n = 10*n
             extra_divisor += 1
         n = int(n)
 
+    ## Attempting to reduce guesses in next session by calling every multitude of 10
+    ## bounded below by 3^<multitude> (approximation for sqrt(10)
+    for i in range(1, n):
+        if 10**i <= n:
+            power += 1
+        else:
+            break
     
-    for i in range(1,n+1):
+    low = 3**power
+
+    ## Finding closest integer that when squared is nearly n
+    for i in range(low,n+1):
         if (i**2) <= n:
             answer = i
             divisor = n // i
             remainder = n - i**2
         else:
-            break
+            break # necessary to avoid checking every integer
 
+
+    ## Going through the actual methods
     while add_zeros != decimal:
         inequality = remainder * 100
         divisor = answer * 2
@@ -38,12 +55,14 @@ def sqrt_ld(n, decimal):
             if ((divisor * 10 + i) * i) <= (inequality):
                 new_divisor = (divisor * 10 + i)
                 next_digit = i
+            else:
+                break # reduces runtime
 
         answer = answer * 10 + next_digit
         remainder = inequality - (new_divisor * next_digit)
         add_zeros += 1
 
-    
+    ## Recursion to handle non-integers
     if extra_divisor != 0:
         extra_divisor = 10**extra_divisor
         extra_divisor = sqrt_ld(extra_divisor, decimal)
@@ -51,6 +70,59 @@ def sqrt_ld(n, decimal):
     else:
         answer = (answer / 10**add_zeros)
 
-    return answer
-                
+    return round(answer, decimal)
+
+#---------------------------------------------------------------------------
+
+## Faster method
+def sqrt_NR(n, decimal):
+    '''Newton-Raphson method (Babylonian method) of calculating square
+    roots using a slight bit of calculus'''
+
+    ## Equation derivation:
+    ## Solve: f(x) = x**2 - n = 0
+    ## f'(x) = 2x
+    ## When f(x) intercepts x axis, distance from tangent line x intercept
+    ## of f(x) to x value be zero (or close to it).
+    ## Step size is equal to f(x)/f'(x)
+    ## Thus, need to solve 0 = x - f(x)/f'(x)
+    ## 0 = x - ((x**2 - n) / (2x)) = x - ((x/2) - (n/2x))
+
+    extra_divisor = 0
+
+    if n == 0:
+       return 0.0
+
+    ## Function gets funky when dealing with numbers less than one
+    if n < 1:
+        while n%1 != 0:
+            n = 10*n
+            extra_divisor += 1
+        n = int(n)
+
+    function = lambda x: x - ((x/2) - (n/2/x))
+    x = n # a square root will never exceed this
+    next_x = function(x)
+
+    while (x - next_x) >= 10**(-decimal):
+        x = next_x
+        next_x = function(x)
+        #print(x, next_x) # if you want to see steps taken
+        #if x == next_x:
+        #    break
+
+
+    ## Same way other function was handled. Iteration for numbers less than one
+    if extra_divisor != 0:
+        extra_divisor = 10**extra_divisor
+        extra_divisor = sqrt_NR(extra_divisor, decimal)
+        answer = (next_x / extra_divisor)
+    else:
+        answer = (next_x)
+
+    return round(answer, decimal)
+
+
+
+
     
