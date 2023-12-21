4d314dca798a0e9b9a2e2a388995fbc1bd4694e2
Magnus <150977273+1000Ball@users.noreply.github.com>
2023-12-21 08:13:05 -0500

    Add files via upload

--- Chord_Diameter.py
+++ Chord_Diameter.py
@@ -6,6 +6,10 @@ def Chord_Diameter(l, h):
     a chord where the length of the chord is known as well as it's perpendicular
     distance from the center of the circle. Returns diameter.'''
 
+    ## If we consider the height intersecting the chord halfway along it's length,
+    ## the hypotenuse of the height/half-length/radius triangle is
+    ## the radius of the sliced circle.
+    ## What we have is r = sqrt(h^2 + (l/2)^2) = sqrt(4h^2 + l^2)/2
     Diameter = np.sqrt(l**2 + (4*h**2))
     Radius = Diameter / 2
 
